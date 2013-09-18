2231fc3b3c21192a2f8b5ee328217d83df4cd684
Mark Watts <mark.watts@hotmail.co.uk>
2013-09-18 17:59:15 +0100

    Tweak sample app to prevent multiple initialisations.

--- src/example/Slumber.Example/App.fs
+++ src/example/Slumber.Example/App.fs
@@ -12,34 +12,40 @@ type App () =
 
     let [<Literal>] LogName = "Slumber.log"
 
-    do
-        XmlConfigurator.Configure (
-            new FileInfo ( 
-                Path.Combine (
-                    AppDomain.CurrentDomain.BaseDirectory,
-                    "log4net.config"
+    static let mutable _initialised = false
+
+    member this.Application_BeginRequest (_ : obj, _ : EventArgs) =
+        if (not _initialised) then
+
+            _initialised <- true
+
+            XmlConfigurator.Configure (
+                new FileInfo ( 
+                    Path.Combine (
+                        AppDomain.CurrentDomain.BaseDirectory,
+                        "log4net.config"
+                    )
                 )
             )
-        )
-        |> ignore
+            |> ignore
 
-        let log = 
-            LogManager.GetLogger LogName
+            let log = 
+                LogManager.GetLogger LogName
 
-        setLogWriter (fun entry ->
-            match entry with
-            | Debug msg -> 
-                if log.IsDebugEnabled then
-                    log.Debug (msg)
-            | Info msg ->
-                if log.IsInfoEnabled then
-                    log.Info (msg)
-            | Warning msg ->
-                if log.IsWarnEnabled then
-                    log.Warn (msg)
-            | Error (msg, err) ->
-                if log.IsErrorEnabled then
-                    match err with 
-                    | Some ex ->  log.Error (msg, ex)
-                    | _ -> log.Error (msg)
-        )
+            setLogWriter (fun entry ->
+                match entry with
+                | Debug msg -> 
+                    if log.IsDebugEnabled then
+                        log.Debug (msg)
+                | Info msg ->
+                    if log.IsInfoEnabled then
+                        log.Info (msg)
+                | Warning msg ->
+                    if log.IsWarnEnabled then
+                        log.Warn (msg)
+                | Error (msg, err) ->
+                    if log.IsErrorEnabled then
+                        match err with 
+                        | Some ex ->  log.Error (msg, ex)
+                        | _ -> log.Error (msg)
+            )
