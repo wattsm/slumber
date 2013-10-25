c297808f71ed3170017108964b2f3a80cf315b27
Mark Watts <mark.watts@hotmail.co.uk>
2013-10-25 08:52:00 +0100

    Change implementation of Common.options to match wiki version.

--- src/example/Slumber.Example/Common.fs
+++ src/example/Slumber.Example/Common.fs
@@ -2,34 +2,27 @@
 
 open System
 open Slumber
-open Slumber.Common.Http
 open Slumber.Framework
 open Slumber.Framework.Core.Containers
-open HandyFS
 
 module Common = 
 
     let options (meta : OperationMetadata) = 
 
-        let container = 
-            ImplicitConfiguration.get meta.Request.Url.BaseUrl
-
         let endpoint = 
-            container
+            meta.ContainerUrl
+            |> ImplicitConfiguration.get
             |> tryGetEndpointByName meta.EndpointName
             |> Option.get
 
         let verbs = 
             endpoint.Bindings
             |> List.map (fun binding -> binding.Verb)
-            |> List.toArray
             
         let accept = 
             String.Join (",", verbs)
 
-        { 
-            OperationResult.Empty 
-            with
-                StatusCode = (Some StatusCodes.Ok);
-                Headers = [ (Headers.Accept, accept); ];
-        }
+        OperationResult.StatusOnly (
+            StatusCodes.Ok,
+            [ (Headers.Accept, accept) ]
+        )
