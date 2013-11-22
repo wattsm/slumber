cf665fbaeca067dd9a94e29ab5cd55942413702b
Mark Watts <mark.watts@hotmail.co.uk>
2013-11-22 19:05:51 +0000

    Add shared assembly info file for versioning.

---
+++ src/app/AssemblyInfo.Common.fs
@@ -0,0 +1,11 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+
+let [<Literal>] private Version = "0.9.0.0"
+
+[<assembly: AssemblyProductAttribute ("Slumber")>]
+[<assembly: AssemblyVersion (Version)>]
+[<assembly: AssemblyFileVersion (Version)>]
+
+do ()
